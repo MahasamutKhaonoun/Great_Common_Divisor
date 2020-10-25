0242f3bb648a00b20b38893f033e11e7173a7ca5
เอกภพ ขาวนวล < @DESKTOP-SURU41J>
2020-10-25 23:38:14 +0700

    finish

--- Great_Common_Divisor/Great_Common_Divisor.cpp
+++ Great_Common_Divisor/Great_Common_Divisor.cpp
@@ -2,28 +2,27 @@
 int Maxnumber(int a, int b);
 int main()
 {
-	long num[2],x[2];
+	long num[2];
+	long* n = num;
 	for (int i = 0; i < 2; i++)
 	{
 		if (scanf_s("%d", &num[i]) != 1)
 		{
 			printf("Error");
-			return 0;
+			return 1;
 		}
-		else if (num[i] <=0 || num[i] > 2000000000)
-		{
-			printf("Error");
-			return 0;
-		}
-		x[i] = num[i];
 	}
 
-	for (int i = Maxnumber(x[0], x[1]); i > 0; i--)
+	for (int i = Maxnumber(*n , *(n+1)) ; i > 0; i--)
 	{
-		if (x[0] % i == 0 && x[1] % i == 0)
+		if (*n % i == 0 && *(n+1) % i == 0)
 		{
 			printf("%d", i);
 			i = 0;
+		}
+		else
+		{
+			return 1;
 		}
 	}
 	return 0;
@@ -31,12 +30,20 @@ int main()
 
 int Maxnumber(int a, int b)
 {
-	if (a >= b)
+	if ((a > 0 && a < 2000000000) && (b > 0 && b < 2000000000))
 	{
-		return a;
+		if (a >= b)
+		{
+			return a;
+		}
+		else
+		{
+			return b;
+		}		
 	}
 	else
 	{
-		return b;
+		printf("Error");
+		return 1;
 	}
 }
